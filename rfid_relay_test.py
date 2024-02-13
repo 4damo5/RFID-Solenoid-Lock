49ceb50c5007807c74d2c824378661818cfd3540
4damo5 <adamotsuka3@gmail.com>
2024-02-13 15:50:54 -0500

    first commit
    it done

---
+++ rfid_relay_test.py
@@ -0,0 +1,95 @@
+from google.oauth2 import service_account
+from Google import Create_Service
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+
+
+
+#relay stuff when u get to it:
+#https://www.youtube.com/watch?v=gJZmbqq2-5U&ab_channel=NerdCave
+
+#got google writing source code from this dude:
+#https://www.youtube.com/watch?v=YF7Ad-7pvks&ab_channel=JieJenn
+#https://www.youtube.com/playlist?list=PL3JVwFmb_BnSee8RFaRPZ3nykuMRlaQp1
+
+#the google sheets bs stuff
+CLIENT_SECRET_FILE = 'client_secret.json'
+
+API_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION, SCOPES)
+
+spreadsheet_id = "YOUR SPREADSHEET ID" #make sure the spreadsheet is set to writer for anyone who has link
+mySpreadsheets = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+
+#more google sheets bs
+worksheet_name = 'Sheet1' #change if the spreadsheet is named somethin different
+
+#ids verified
+valid_ids = [] #enter your tag ids
+
+#reading var 
+reader = SimpleMFRC522()
+
+#relay stuffs
+GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+#start closed
+GPIO.output(18,0)
+
+#gives a start time relative to current time
+start = time.monotonic()
+
+def data_send(t,i,tx):
+    values = [
+    [t,i,tx]
+    ]
+
+    #append it as columns
+    value_range_body = {
+        'values': values
+    }
+
+    #do the stuff in the sheet wooo magic
+    service.spreadsheets().values().append(
+    spreadsheetId=spreadsheet_id,
+    valueInputOption='USER_ENTERED',
+    range=worksheet_name,
+    body=value_range_body
+    ).execute()
+
+#test loop to see if everything works
+for i in range(0,4):
+    print("Place tag: ")
+    try:
+        id, text = reader.read()
+
+        #keep track of the time after id is read
+        end = time.monotonic()
+        
+        #if the id is valid, open lock
+        if id in valid_ids:
+            gate = True
+            GPIO.output(18,1)
+
+        else:
+            print('invalid id')
+
+    finally:
+    #do the end stuff 
+        data_send(end - start, id,text)
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+    
+    #end closed
+    time.sleep(2)
+    GPIO.output(18,0)
+    time.sleep(2)
